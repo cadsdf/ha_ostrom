6b3e0c8a97421565bc16d6279e01f7c259542baf
susanne <67089533+melmager@users.noreply.github.com>
2025-08-20 10:17:15 +0200

    Update const.py
    v4.0

--- custom_components/ostrom/const.py
+++ custom_components/ostrom/const.py
@@ -5,3 +5,8 @@ DOMAIN = "ostrom"
 # Icons
 ICON = "mdi:current-ac"
 ICON_COST = "mdi:currency-eur"
+ICON_PLUG = "mdi:power-plug"
+ICON_GRID = "mdi:transmission-tower"
+ICON_CASH = "mdi:account-cash-outline"
+
+
